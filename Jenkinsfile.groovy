ad1db908ee90710d5ccb2c76c03242ba396edb90
Prasath Sivasubramaniyan <tsprasath91@gmail.com>
2017-09-03 02:28:40 -0500

    Update and rename jenkinsfile.groovy to Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,15 +1,16 @@
 node {
+ wrap([$class: 'AnsiColorBuildWrapper', colorMapName: 'xterm']) {
  
     // Mark the code checkout 'Checkout'....
     stage 'Checkout'
  
     // // Get some code from a GitHub repository
-    git url: 'git@github.com:myorg/infrastructure.git'
+    git url: 'https://github.com/tsprasath/s3.git'
  
     // Get the Terraform tool.
     def tfHome = tool name: 'terraform', type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool'
     env.PATH = "${tfHome}:${env.PATH}"
-    wrap([$class: 'AnsiColorBuildWrapper', colorMapName: 'xterm']) {
+    
  
             // Mark the code build 'plan'....
             stage name: 'Plan', concurrency: 1
@@ -22,9 +23,11 @@ node {
             if (fileExists("status")) {
                 sh "rm status"
             }
-            sh "init.sh"
+            //sh "init.sh"
             sh "terraform get"
-            sh "set +e; terraform plan -out=plan.out -detailed-exitcode; echo \$? &gt; status"
+            sh "terraform init"
+            sh "terraform providers"
+              sh "set +e; terraform plan -out=plan.out -detailed-exitcode"
             def exitCode = readFile('status').trim()
             def apply = false
             echo "Terraform Plan Exit Code: ${exitCode}"
@@ -32,17 +35,17 @@ node {
                 currentBuild.result = 'SUCCESS'
             }
             if (exitCode == "1") {
-                slackSend channel: '#ci', color: '#0080ff', message: "Plan Failed: ${env.JOB_NAME} - ${env.BUILD_NUMBER} ()"
+                //slackSend channel: '#ci', color: '#0080ff', message: "Plan Failed: ${env.JOB_NAME} - ${env.BUILD_NUMBER} ()"
                 currentBuild.result = 'FAILURE'
             }
             if (exitCode == "2") {
                 stash name: "plan", includes: "plan.out"
-                slackSend channel: '#ci', color: 'good', message: "Plan Awaiting Approval: ${env.JOB_NAME} - ${env.BUILD_NUMBER} ()"
+                //slackSend channel: '#ci', color: 'good', message: "Plan Awaiting Approval: ${env.JOB_NAME} - ${env.BUILD_NUMBER} ()"
                 try {
                     input message: 'Apply Plan?', ok: 'Apply'
                     apply = true
                 } catch (err) {
-                    slackSend channel: '#ci', color: 'warning', message: "Plan Discarded: ${env.JOB_NAME} - ${env.BUILD_NUMBER} ()"
+                    //slackSend channel: '#ci', color: 'warning', message: "Plan Discarded: ${env.JOB_NAME} - ${env.BUILD_NUMBER} ()"
                     apply = false
                     currentBuild.result = 'UNSTABLE'
                 }
@@ -57,9 +60,9 @@ node {
                 sh 'set +e; terraform apply plan.out; echo \$? &amp;gt; status.apply'
                 def applyExitCode = readFile('status.apply').trim()
                 if (applyExitCode == "0") {
-                    slackSend channel: '#ci', color: 'good', message: "Changes Applied ${env.JOB_NAME} - ${env.BUILD_NUMBER} ()"    
+                    //slackSend channel: '#ci', color: 'good', message: "Changes Applied ${env.JOB_NAME} - ${env.BUILD_NUMBER} ()"    
                 } else {
-                    slackSend channel: '#ci', color: 'danger', message: "Apply Failed: ${env.JOB_NAME} - ${env.BUILD_NUMBER} ()"
+                    //slackSend channel: '#ci', color: 'danger', message: "Apply Failed: ${env.JOB_NAME} - ${env.BUILD_NUMBER} ()"
                     currentBuild.result = 'FAILURE'
                 }
             }
